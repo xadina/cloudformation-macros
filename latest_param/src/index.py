e2ac94654d71255b59e75b6a6de90c15ce64664b
Jonathan Bugeja <jonathan.bugeja@casumo.com>
2020-02-24 09:03:06 +0100

    small fixes

--- latest_param/src/index.py
+++ latest_param/src/index.py
@@ -1,29 +1,30 @@
 import boto3
 
-client = boto3.client('ssm')
+client = boto3.client("ssm")
+
 
 def extract_value(params):
-    status = 'success'
-    match = 'false'
-    key = params['parameter']
+    status = "success"
+    key = params["parameter"]
     try:
-        details = client.get_parameter(Name=key,WithDecryption=True)
+        details = client.get_parameter(Name=key, WithDecryption=True)
         try:
-            value = details['Parameter']['Value']
+            value = details["Parameter"]["Value"]
         except:
-            print('Unable to extract value for parameter ' + key)
-            status = 'failure'
-            value = 'NotAvailable'
+            print("Unable to extract value for parameter " + key)
+            status = "failure"
+            value = "NotAvailable"
     except:
-        print('parameter ' + key + ' not found')
-        status = 'failure'
-        value = 'NotFound'
+        print("parameter " + key + " not found")
+        status = "failure"
+        value = "NotFound"
     return status, value
 
+
 def handler(event, context):
-    result = extract_value(event['params'])
+    result = extract_value(event["params"])
     return {
-        'requestId': event['requestId'],
-        'status': result[0],
-        'fragment': result[1],
+        "requestId": event["requestId"],
+        "status": result[0],
+        "fragment": result[1],
     }
